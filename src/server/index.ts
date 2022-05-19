63c944bda3ef534c4643b3dbd656fa1882cea14b
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-05-19 20:17:36 -0300

    chore: add ejs

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,11 +7,20 @@ const app = express();
 const server = http.createServer(app);
 const io = new socketio.Server(server);
 
-app.use(express.static(path.resolve(__dirname, '..', 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '..', 'public'));
 app.use(express.static(path.resolve(__dirname, '../..', 'dist')));
 
 io.on('connection', (socket) => {
   console.log(socket.id);
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/play', (req, res) => {
+  res.render('play');
 });
 
 server.listen(3333);
