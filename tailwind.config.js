f71d5820ece0a450c61cd94f8b562623d0132ce6
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-05-20 16:13:15 -0300

    chore: add font Mali

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ['./src/**/*.ejs'],
   theme: {
+    extend: {
+      fontFamily: {
+        mali: ['Mali', 'cursive'],
+      },
+    },
     linearBorderGradients: (theme) => ({
       colors: {
         'black-white': ['#000 50%', '#fff'],
