06810655dbb4c2bad7bad92da21e6d78b016e222
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-06-02 20:23:11 -0300

    fix: add `structuredClone` declaration

---
+++ apps/web/src/scripts/Types/structuredClone.d.ts
@@ -0,0 +1,7 @@
+interface WindowOrWorkerGlobalScope {
+  structuredClone(value: any, options?: StructuredSerializeOptions): any;
+}
+declare function structuredClone(
+  value: any,
+  options?: StructuredSerializeOptions,
+): any;
